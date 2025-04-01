2dfdc69cfa53b94a24b4b8b99f5ee50ca699d6b6
iceriny <suqianchengice@gmail.com>
2025-04-01 10:35:33 +0800

    -

---
+++ src/plugins/command/__init__.py
@@ -0,0 +1,26 @@
+from nonebot.adapters.onebot.v11 import Bot
+from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.adapters.onebot.v11.message import MessageSegment
+from nonebot.log import logger
+from nonebot import get_plugin_config, on_command
+from nonebot.plugin import PluginMetadata
+from src.plugins.command.config import Config
+
+
+__plugin_meta__ = PluginMetadata(
+    name="command",
+    description="处理命令的模块",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+
+command_matcher = on_command("cmd", priority=config.command_priority)
+
+
+@command_matcher.handle()
+async def handle_receive(bot: Bot, event: MessageEvent):
+    logger.debug(str(bot), event)
+    await command_matcher.finish()
